16ee46c9b50db37a7eef8d05d03f7c172f78fe58
Angel Laguna <al4guna@gmail.com>
2021-08-04 08:04:35 -0500

    v1-base - developing => component Footer

---
+++ src/components/FooterOmni/FooterOmni.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Footer} from "./styled";
+
+const FooterOmni = () => {
+    return (
+        <Footer>
+            <p>© Copyright</p>
+        </Footer>
+    )
+}
+
+export default FooterOmni;
---
+++ src/components/FooterOmni/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+    color: #fff;
+    height: 70px;
+    display: flex;
+    padding: 0 20px;
+    align-items: center;
+    background: #33333A;
+`
